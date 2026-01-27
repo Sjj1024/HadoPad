651a58328c4b72aec199f51cd530912ba43d1734
songjj <shongjj@meleapcn.com>
2026-01-27 17:22:47 +0800

    Add QR code scanning functionality with dedicated view controller and SwiftUI integration

---
+++ HadoPad/Utils/QRControl.swift
@@ -0,0 +1,191 @@
+import AVFoundation
+import UIKit
+
+// 协议定义：二维码扫描器视图控制器的委托
+protocol QRCodeScannerViewControllerDelegate: AnyObject {
+    func didFindCode(_ code: String)
+}
+
+// QRCodeScannerViewController 类：用于扫描二维码的视图控制器
+class QRCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    weak var delegate: QRCodeScannerViewControllerDelegate?
+    var scanRectView: UIView!
+    var overlayView: UIView!
+    var isScanningEnabled: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        setupOverlay()
+        setupScanRect()
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+        }
+    }
+    
+    func setupOverlay() {
+        overlayView = UIView(frame: view.bounds)
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        let path = UIBezierPath(rect: overlayView.bounds)
+        let scanRectSize: CGFloat = 200
+        let scanRect = CGRect(
+            x: (view.frame.width - scanRectSize) / 2,
+            y: (view.frame.height - scanRectSize) / 2,
+            width: scanRectSize,
+            height: scanRectSize
+        )
+        let scanRectPath = UIBezierPath(roundedRect: scanRect, cornerRadius: 20)
+        path.append(scanRectPath.reversing())
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = path.cgPath
+        overlayView.layer.mask = maskLayer
+        
+        view.addSubview(overlayView)
+    }
+    
+    func setupScanRect() {
+        let scanRectSize: CGFloat = 220
+        let scanRect = CGRect(
+            x: (view.frame.width - scanRectSize) / 2,
+            y: (view.frame.height - scanRectSize) / 2,
+            width: scanRectSize,
+            height: scanRectSize
+        )
+        
+        scanRectView = UIView(frame: scanRect)
+        
+        // Adding corner indicators
+        let cornerLength: CGFloat = 20
+        let cornerThickness: CGFloat = 4
+        let cornerColor = UIColor.green
+                
+        // Top-left corner
+        addCorner(to: scanRectView, x: 0, y: 0, width: cornerLength, height: cornerThickness, color: cornerColor) // Horizontal
+        addCorner(to: scanRectView, x: 0, y: 0, width: cornerThickness, height: cornerLength, color: cornerColor) // Vertical
+                
+        // Top-right corner
+        addCorner(to: scanRectView, x: scanRectSize - cornerLength, y: 0, width: cornerLength, height: cornerThickness, color: cornerColor) // Horizontal
+        addCorner(to: scanRectView, x: scanRectSize - cornerThickness, y: 0, width: cornerThickness, height: cornerLength, color: cornerColor) // Vertical
+                
+        // Bottom-left corner
+        addCorner(to: scanRectView, x: 0, y: scanRectSize - cornerThickness, width: cornerLength, height: cornerThickness, color: cornerColor) // Horizontal
+        addCorner(to: scanRectView, x: 0, y: scanRectSize - cornerLength, width: cornerThickness, height: cornerLength, color: cornerColor) // Vertical
+                
+        // Bottom-right corner
+        addCorner(to: scanRectView, x: scanRectSize - cornerLength, y: scanRectSize - cornerThickness, width: cornerLength, height: cornerThickness, color: cornerColor) // Horizontal
+        addCorner(to: scanRectView, x: scanRectSize - cornerThickness, y: scanRectSize - cornerLength, width: cornerThickness, height: cornerLength, color: cornerColor) // Vertical
+                
+        view.addSubview(scanRectView)
+    }
+    
+    func addCorner(to view: UIView, x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, color: UIColor) {
+        let corner = UIView(frame: CGRect(x: x, y: y, width: width, height: height))
+        corner.backgroundColor = color
+        view.addSubview(corner)
+    }
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if captureSession?.isRunning == false {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.captureSession.startRunning()
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if captureSession?.isRunning == true {
+            captureSession.stopRunning()
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+//        captureSession.stopRunning()
+        print("metadataOutput")
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            print("metadataObject stringValue", stringValue)
+            delegate?.didFindCode(stringValue)
+        }
+        
+        dismiss(animated: true)
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+        }
+    }
+
+    // 开启扫描
+    func startScanning() {
+        dismiss(animated: true)
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+        }
+    }
+
+    // 关闭扫描
+    func stopScanning() {
+        captureSession.stopRunning()
+        print("stopScanning")
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
---
+++ HadoPad/Views/AddUser.swift
@@ -0,0 +1,52 @@
+//
+//  Scanner.swift
+//  HadoPad
+//
+//  Created by song on 2025/11/18.
+//
+
+import SwiftUI
+
+struct Scanner: View {
+    
+    // back button
+    @Environment(\.presentationMode) var presentationMode
+
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            HStack(alignment: .center) {
+                Text("< 返回")
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .onTapGesture {
+                        print("Back button tapped")
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                Spacer()
+            }
+            .padding(.leading, 20)
+            .padding(.top, 20)
+            VStack(spacing: 20) {
+                Spacer()
+                Image("logo").resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 150)
+                Image("huangbt")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 360)
+                Spacer()
+            }
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("huangbg").resizable().scaledToFill())
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    Scanner()
+}
